f8fcdf8af36f17c1ebfdfa7acd9eb1ab902f00ab
peter-fullstack <peter.fullstack@gmail.com>
2026-05-25 13:14:06 +1000

    Updated auth provider

--- src/authConfig.ts
+++ src/authConfig.ts
@@ -2,9 +2,9 @@ import { PublicClientApplication } from '@azure/msal-browser'
 
 const msalConfig = {
   auth: {
-    clientId: 'ef135db6-4729-4c5e-a0cc-0318be619a8f', // your client ID
+    clientId: 'bb47349e-e638-4a37-a082-fd2137bee380', // your client ID
     authority:
-      'https://login.microsoftonline.com/77d7974c-b578-4225-bcfa-7907a2ed40bf', // your tenant ID
+      'https://myobintegrationcustomers.ciamlogin.com/0dcb505d-c3a8-4a5e-945f-e900363cf2b6', // your tenant ID
     redirectUri: 'http://localhost:3000' // or your app's redirect URI
   },
   cache: {
